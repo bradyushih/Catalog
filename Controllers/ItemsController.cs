d4286de0a9fc4f6669d48f790b2cb587332afaee
Brad Shih <brad.shih@cloud-interactive.com>
2023-02-13 16:29:41 +0800

    update async avoiding make locking call

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -23,14 +23,14 @@ namespace Catalog.Controllers
         [HttpGet]
         public IEnumerable<ItemDto> GetItems()
         {
-            var items = repository.GetItems().Select( item => item.AsDto());
+            var items = repository.GetItemsAsync().Select( item => item.AsDto());
             return items;
         }
 
         [HttpGet("{id}")]
         public ActionResult<ItemDto> GetItem(Guid id)
         {
-            var item = repository.GetItem(id);
+            var item = repository.GetItemAsync(id);
 
             if(item is null)
             {
@@ -50,7 +50,7 @@ namespace Catalog.Controllers
                 CreatedDate = DateTimeOffset.UtcNow,
             };
 
-            repository.CreateItem(item);
+            repository.CreateItemAsync(item);
 
             return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
         }
@@ -58,7 +58,7 @@ namespace Catalog.Controllers
         [HttpPut("{id}")]
         public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
         {
-            var existingItem = repository.GetItem(id);
+            var existingItem = repository.GetItemAsync(id);
             
             if(existingItem is null)
             {
@@ -70,7 +70,7 @@ namespace Catalog.Controllers
                 Price = itemDto.Price,
             };
 
-            repository.UpdateItem(updatedItem);
+            repository.UpdateItemAsync(updatedItem);
 
             return NoContent();
         }
@@ -78,14 +78,14 @@ namespace Catalog.Controllers
         [HttpDelete("{id}")]
         public ActionResult DeleteItem(Guid id)
         {
-            var existingItem = repository.GetItem(id);
+            var existingItem = repository.GetItemAsync(id);
 
             if(existingItem is null)
             {
                 return NotFound();
             }
 
-            repository.DeleteItem(id);
+            repository.DeleteItemAsync(id);
 
             return NoContent();
         }
